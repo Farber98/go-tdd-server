37f7854b11e273fb1f68cd871bec9ca25a785754
Farber98 <juantuc98@gmail.com>
2022-05-16 12:59:52 -0300

    first test

--- server_test.go
+++ server_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Yoni's score", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "/players/Yoni", nil)
+		resp := httptest.NewRecorder()
+
+		PlayerServer(resp, req)
+
+		got := resp.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
